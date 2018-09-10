efe74b15d1b192899e31dc9d6d90946210b77def
eegodinez <edgarenrique.godinez@gmail.com>
2018-09-10 09:05:00 -0600

    Update constants.ts

--- Query/lib/controllers/constants.ts
+++ Query/lib/controllers/constants.ts
@@ -1,7 +1,7 @@
-export let matchingURI = "http://34.221.39.159:8080/",
-        exclusionsURI = "http://34.221.39.159:8081/",
-        targetingURI = "http://34.221.39.159:8082/",
-        rankingURI = "http://34.221.39.159:8083/",
-        adsURI = "http://34.221.39.159:8084/",
-        pricingURI = "http://34.221.39.159:8085/"
+export let matchingURI = "http://34.214.164.18:8080/",
+        exclusionsURI = "http://34.214.164.18:8081/",
+        targetingURI = "http://34.214.164.18:8082/",
+        rankingURI = "http://34.214.164.18:8083/",
+        adsURI = "http://34.214.164.18:8084/",
+        pricingURI = "http://34.214.164.18:8085/"
         ;
