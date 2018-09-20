8b1a9373fec6a2cb8c5761ed1fd16186a51aa13f
eegodinez <edgarenrique.godinez@gmail.com>
2018-09-19 18:46:47 -0600

    ms LB

--- Matching/lib/models/mysqlDB.ts
+++ Matching/lib/models/mysqlDB.ts
@@ -1,7 +1,7 @@
 import * as mysql from 'mysql';
 
 let mysql_connection = mysql.createConnection({
-    host     : 'mysqldatabase.cp5tg8bwqbz7.us-east-1.rds.amazonaws.com',
+    host     : 'mysqldbinstance2.ctergpgkzn3p.us-west-2.rds.amazonaws.com',
     user     : 'masterdb',
     password : 'advertisers2018',
     database : 'configuration'
