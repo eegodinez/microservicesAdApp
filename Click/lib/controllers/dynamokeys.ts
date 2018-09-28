f9e7a23744a63813d95b4ab95e627215ecd46e1b
LindaEliza <lmoralesh@ufm.edu>
2018-09-28 15:46:35 -0600

    Ignorar dynamo

--- Click/lib/controllers/dynamokeys.ts
+++ Click/lib/controllers/dynamokeys.ts
@@ -1,6 +1,6 @@
 export default 
 {
-    "user": "DynamoDB",
-    "access": "AKIAIOYFHGVCNDBYCC2A",
-    "secret": "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"
+    "user": " ",
+    "access": " ",
+    "secret": " "
 }
